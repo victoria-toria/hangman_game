082b824646b0468769a3bb0637d2fd06ab6dfe28
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-04-06 18:29:22 +0200

    alphabet btns and hidden word

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-let alphabetDiv = document.querySelector(".alphabet");
-console.log(alphabetDiv);
+//Game area. Alphabet btns and Hidden word
 
-let alphaArr = ["A", "B", "C"];
+let alphabetDiv = document.querySelector(".alphabet");
+let alphaArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 function createBtn(letter) {
   let btn = document.createElement("button");
@@ -14,6 +41,17 @@ alphaArr.forEach((letter) => {
 });
 console.log(alphabetDiv);
 
+let hiddenWordDiv = document.querySelector(".hiddenWord");
 
+let word = "javascript";
 
+function createHiddenWd(word) {
+  let span = document.createElement("span");
 
+  let letterHiddenWord = word.split("");
+  for (let i of letterHiddenWord) {
+    span.innerText += i;
+  }
+  document.querySelector(".hiddenWord").appendChild(span);
+}
+createHiddenWd(word);
