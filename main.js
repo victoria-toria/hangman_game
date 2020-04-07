c04a17f65cda68dad44484b62061616fbbe95b18
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-04-07 15:07:10 +0200

    alphabet btns onclick

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-//Game area. Alphabet btns and Hidden word
+//Game area. Alphabet btns
 
 let alphabetDiv = document.querySelector(".alphabet");
+let btns = document.querySelectorAll("button");
+let word = document.querySelector(".hiddenWord");
+
+const allButtons = Array.from(btns);
+console.log(allButtons);
+
 let alphaArr = [
   "A",
   "B",
@@ -41,17 +47,77 @@ alphaArr.forEach((letter) => {
 });
 console.log(alphabetDiv);
 
-let hiddenWordDiv = document.querySelector(".hiddenWord");
-
-let word = "javascript";
+////Hidden word
 
 function createHiddenWd(word) {
-  let span = document.createElement("span");
-
   let letterHiddenWord = word.split("");
-  for (let i of letterHiddenWord) {
-    span.innerText += i;
-  }
-  document.querySelector(".hiddenWord").appendChild(span);
+  let spanArr = letterHiddenWord.map(function (el) {
+    return "<span>" + el + "</span>";
+  });
+  let str = spanArr.join("");
+  console.log(str);
+  document.querySelector(".hiddenWord").innerHTML = str;
 }
-createHiddenWd(word);
+createHiddenWd("javascript");
+
+//Initial value of alphabet and hidden word
+let state = {
+  btns: false,
+  word: false,
+};
+console.log("hey");
+console.log("banana");
+
+//Buttons onclick event
+allButtons.forEach((btn) => {
+  btn.addEventListener("onclick", () => {
+    console.log("hey");
+  });
+});
+
+function addRemoveActive(button, state) {
+  if (state) {
+    button.classList.add("active");
+  } else {
+    button.classList.remove("active");
+  }
+}
+
+//=> need to get to word(i)
+function checkIfContains() {
+  for (let i = 0; i < alphabetDiv.length; i++) {
+    for (let j = 0; j < word.length; j++) {
+      if (i === j) {
+        console.log("yep");
+        //   word(i).style.visibility = "visible"
+      } else {
+        console.log("nope");
+        //   word(i).style.visibility = "hidden"
+      }
+    }
+  }
+}
+checkIfContains();
+
+//function render hidden word
+
+// function renderHiddenWord(word) {
+//   for (i = 0; i < word.length; i++) {
+//     if (true) {
+//       word(i).style.visibility = "visible";
+//     } else {
+//       word(i).style.visibility = "hidden";
+//     }
+//   }
+// }
+
+///Onclick change color
+
+// document.getElementById("button").addEventListener("click", function(el){
+//   if (count == 0) {
+//     el.style.color = "#ff0000";
+//     count = 1;
+//   } else {
+//     el.style.color = "#7FFF00";
+//     count = 0;
+//   })
