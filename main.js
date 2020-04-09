18a6cb6140c6f95c9d8ed571afd069db003822c3
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-04-09 15:22:04 +0200

    ready game without hangman pics

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let wordDiv = document.querySelector(".hiddenWord");
 let guessWord = "JAVASCRIPT";
 let guessWordLength = guessWord.length;
 console.log(guessWordLength);
-let lives = 10;
+let lives = 7;
 
 let alphaArr = [
   "A",
@@ -36,6 +36,11 @@ let alphaArr = [
   "Z",
 ];
 
+// let correctSound = document.createElement("audio");
+// let incorrectSound = document.createElement("audio");
+// correctSound.setAttribute("src", "/sounds/CAR-DOOR.mp3");
+// incorrectSound.setAttribute("src", "/sounds/CAR-DOOR.mp3");
+
 function createBtn(letter) {
   let btn = document.createElement("button");
   btn.innerText = letter;
@@ -53,11 +58,14 @@ function createHiddenWd() {
   let letterHiddenWord = guessWord.toString().split("");
   console.log(letterHiddenWord);
   letterHiddenWord.forEach(function (el) {
+    let divSpanLetter = document.createElement("div");
     let spanLetter = document.createElement("span");
     console.log(spanLetter);
+    divSpanLetter.style.borderBottom = "thick solid #000000";
     spanLetter.innerText = el;
     spanLetter.classList.add("ltr", el);
-    wordDiv.appendChild(spanLetter);
+    divSpanLetter.appendChild(spanLetter);
+    wordDiv.appendChild(divSpanLetter);
   });
 }
 
@@ -79,6 +87,7 @@ let wrongAnswersArr = [];
 
 function checkIfContains(letter) {
   let letterFound;
+
   guessWord.split("").forEach((elem) => {
     console.log(elem, letter);
     if (elem == letter) {
@@ -86,6 +95,7 @@ function checkIfContains(letter) {
       let nodeEl = document.getElementsByClassName(letter);
       [...nodeEl].forEach((l) => {
         l.classList.remove("ltr");
+        // correctSound.play();
       });
       letterFound = true;
       rightAnswersArr.push(elem);
@@ -93,14 +103,27 @@ function checkIfContains(letter) {
   });
   if (!letterFound) {
     wrongAnswersArr.push(letter);
+    // correctSound.play();
   }
 
-  lives = 7 - wrongAnswersArr.length;
+  lives = wrongAnswersArr.length;
   if (wrongAnswersArr.length >= 7) {
+    updateDisplayLose();
     console.log("Gameover");
   }
   if (rightAnswersArr.length == guessWordLength) {
+    updateDisplayWin();
     console.log("You WIN!");
   }
   console.log(wrongAnswersArr, rightAnswersArr);
+}
+
+function updateDisplayLose() {
+  let gameover = "Gameover";
+  document.getElementsByClassName("hiddenWord")[0].innerText = gameover;
+}
+
+function updateDisplayWin() {
+  let youWin = "You win!";
+  document.getElementsByClassName("hiddenWord")[0].innerText = youWin;
 }
